daf1c7a8c950869e578f176390c254f5f6614ccc
Pablo Climent <chinso@gmail.com>
2023-02-04 07:22:05 +0100

    add all files inside cpp /dist/ to gitignore Hello word to test cpp23 template and compiler tasks

---
+++ C and C++/C++/C++ Programming Course - Beginner to Advanced/Hello_world/README.md
@@ -0,0 +1,9 @@
+# C++
+
+## About this repositorie
+
+Tipical Hello World to check VScode compilers configuration
+
+- [x] Compile with GCC
+- [x] Compile with Clang
+- [x] Compile with Microsoft compiler cl
---
+++ C and C++/C++/C++ Programming Course - Beginner to Advanced/Hello_world/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+
+- [x] Compile with GCC
+- [x] Compile with Clang
+- [x] Compile with Microsoft compiler cl
+
+*/
+
+int main(){
+
+  std::cout << "Hello Worldin C++ 23!" << std::endl;
+   
+}
