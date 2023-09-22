cb9d679f39785f1e7404b9adb83777664fde1a8d
pchinso <chinso@gmail.com>
2023-09-22 12:23:47 +0200

    add dict based screens creation an navigation for st.image errors handling

--- Python/OpenCV/OpenCv_StreamLitApp/OOP_Streamlit_test.py
+++ Python/OpenCV/OpenCv_StreamLitApp/OOP_Streamlit_test.py
@@ -7,40 +7,81 @@ class StreamlitApp:
                         "About", 
                         "Contact",
                         ]
-        self.navigation = st.sidebar.radio("Select Screen", self.screens)
+        
+        self.screens = [
+        {
+        'title': 'Home',
+        'header': 'Home Screen',
+        'write': 'Welcome to the home screen!',
+        'image': 'images/Me_profile_pic.jpeg',
+        'image_caption': 'Home image',
+        },
+        {
+        'title': 'About',
+        'header': 'About Screen',
+        'write': 'This is the about screen.',
+        'image': 'images/Me_profile_pic.jpeg',
+        'image_caption': 'About image',
+        },
+        {
+        'title': 'Contact',
+        'header': 'Contact Screen',
+        'write': 'You can contact us at contact@example.com',
+        'image': 'images/Me_profile_pic.jpeg',
+        'image_caption': 'Contact image',
+        },
+        {
+        'title': 'Send',
+        'header': 'Send Results',
+        'write': 'Sending results to contact@example.com',
+        'image': '',
+        'image_caption': 'No Image',
+        },        
+        ]
+
+        self.radio_buttons = [screen['title'] for screen in self.screens]
+
+
+        self.navigation = st.sidebar.radio("Select Screen", 
+                                           self.radio_buttons
+                                           )
 
     def run(self):
-        st.title("Object-Oriented Streamlit App")
-        self.show_navigation()
 
-    def show_navigation(self):
-                
-        if self.navigation in self.screens:
-            self.show_screen()
+        # Main App Title
+        st.title("Object-Oriented Streamlit App")
+        
+        self.show_screen()
             
 
 
     def show_screen(self):
 
-        if self.navigation == "Home":
-            
-            st.header("Home Screen")
-            st.write("Welcome to the home screen!")
-            st.image("images/Me_profile_pic.jpeg", caption="Home Image")
+        # Index of actual navigation button selected
+        screen_index = next((
+        screen_index
+        for screen_index, screen in enumerate(self.screens)
+        if screen['title'] == self.navigation
+        ), None)   
 
-        elif self.navigation == "About":
-            st.header("About Screen")
-            st.write("This is the about screen.")
-            st.image("images/Me_profile_pic.jpeg", caption="About Image")            
-
-        elif self.navigation == "Contact":
-            st.header("Contact Screen")
-            st.write("You can contact us at contact@example.com")
-            st.image("images/Me_profile_pic.jpeg", caption="Contact Image")            
+        # Shows screen data
+        st.header(self.screens[screen_index]['header'])
+        st.write(self.screens[screen_index]['write'])
         
+        if self.screens[screen_index]['image'] != '':
+            try:
+                st.image(self.screens[screen_index]['image'], 
+                        caption=self.screens[screen_index]['image_caption']
+                        )
+                
+            except FileNotFoundError:
+                st.error("File not found. Please provide a valid file path.")      
+
+            
         # Update current screen
         self.current_screen = self.navigation
         
 if __name__ == "__main__":
+
     app = StreamlitApp()
     app.run()
