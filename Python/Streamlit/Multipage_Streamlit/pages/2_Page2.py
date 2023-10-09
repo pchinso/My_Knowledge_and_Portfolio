2d0bf31f1ddb28ba0c3f1c604eaf5eadfe402dfb
pchinso <chinso@gmail.com>
2023-10-09 15:14:52 +0200

    Testing Streamlit multipage

---
+++ Python/Streamlit/Multipage_Streamlit/pages/2_Page2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+st.set_page_config(page_title="Page 2", page_icon=None)
+
+st.markdown("# Page 2")
+st.sidebar.header("Page 2 Sidebar")
+st.markdown(
+    """ssssss"""
+)
