3d7de2b5d22dbbd253df5e6429ca638c854ec124
Pablo Climent <chinso@gmail.com>
2023-12-16 10:38:12 +0100

    reflex linksweb with sidebar initialization

---
+++ Python/Reflex/links_web/links_web/links_web.py
@@ -0,0 +1,12 @@
+"""Welcome to Reflex!."""
+
+from links_web import styles
+
+# Import all the pages.
+from links_web.pages import *
+
+import reflex as rx
+
+# Create the app and compile it.
+app = rx.App(style=styles.base_style)
+app.compile()
