f425fc8d8c625717c9182625102e5a8f297c9198
pchinso <chinso@gmail.com>
2023-12-20 08:41:57 +0100

    add upload box

--- Python/Reflex/links_web/links_web/pages/index.py
+++ Python/Reflex/links_web/links_web/pages/index.py
@@ -6,13 +6,21 @@ from links_web.templates import template
 import reflex as rx
 
 
-@template(route="/", title="Home", image="/github.svg")
+@template(route="/", title="Upload", image="/upload.svg")
 def index() -> rx.Component:
     """The home page.
 
     Returns:
         The UI for the home page.
     """
-    with open("README.md", encoding="utf-8") as readme:
-        content = readme.read()
-    return rx.markdown(content, component_map=styles.markdown_style)
+    # with open("README.md", encoding="utf-8") as readme:
+    #     content = readme.read()
+    # return rx.markdown(content, component_map=styles.markdown_style)
+
+    return rx.upload(
+        rx.text(
+            "Drag and drop files here or click to select files"
+        ),
+        border="1px dotted rgb(107,99,246)",
+        padding="5em",
+    )
